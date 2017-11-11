353c35b681db55239095a84940d68678f719641b
Franco Masotti <franco.masotti@live.com>
2017-11-11 15:27:10 +0100

    Added tests. Fixed the get_md_heading method.

--- md_toc/api.py
+++ md_toc/api.py
@@ -10,7 +10,14 @@ def build_toc_line(header):
     """
 
     assert isinstance(header, dict)
+    assert 'type' in header
+    assert 'text_original' in header
+    assert 'text_slugified' in header
+    assert isinstance(header['type'],int)
+    assert isinstance(header['text_original'],str)
+    assert isinstance(header['text_slugified'],str)
 
+    return toc_line
 
 
 def get_md_heading(line):
@@ -23,28 +30,36 @@ def get_md_heading(line):
     line = line.strip()
 
     # 2. If first char of line is not '#' return None. This means
-    # we don't care about this line
+    # we don't care about this line. Check also if it's an empty string.
+    if len(line) == 0:
+        return None
     if line[0] != '#':
         return None
 
     # 3. Remove the leading '#'s.
     header_text = line.lstrip('#')
 
-    # 4. Count the number of '#' character to determine what kind of title it
-    #    is.
+    # 4. Count the number of leading '#' character to determine what kind of
+    # title it is.
     #
     #    Waring: This is computationally unconvenient: O(n) where n =
     #    len(line). It is however more elegant than iterating on the line
     #    itself.
     header_type = len(line)-len(header_text)
 
-    # 5. Remove possible whitespace after removing the '#'s.
-    header_text.strip()
+    # 5. If it's a 4 (or more) title level we will ignore it.
+    if header_type > 3:
+        return None
 
-    # 6. Return a dict with the two data sets we need.
+    # 6. Remove possible whitespace after removing the '#'s.
+    header_text = header_text.strip()
+
+    # 7. Return a dict with the three data sets we need. Note that we need the
+    # title text to be slugified so it can be used as link.
     header = {
-               type: header_type,
-               text: slugify(header_text)
+               'type': header_type,
+               'text_original': header_text,
+               'text_slugified': slugify(header_text)
              }
     return header
 
--- tests/tests.py
+++ tests/tests.py
@@ -1,2 +1,64 @@
 from md_toc import api
+from slugify import slugify
+import string
+import random
 import unittest
+import sys
+
+ITERATION_TESTS = 1024
+RANDOM_STRING_LENGTH = 512
+
+class TestArgumentParser(unittest.TestCase):
+
+    def test_m_get_md_header(self):
+        test_text = '# #ciao # bau'
+        test_text = test_text.lstrip('#')
+#        test_text = test_text.strip()
+        print(test_text)
+
+        self.assertEqual(api.get_md_heading('#' + test_text),
+           {'type':1,'text_original':test_text.strip(),'text_slugified':slugify(test_text)})
+
+    def test_get_md_header(self):
+        i = 0
+        while i < ITERATION_TESTS:
+            test_text = ''.join([random.choice(string.printable) for n in range(RANDOM_STRING_LENGTH)])
+            # Remove any leading '#' character that might pollute the tests.
+            test_text = test_text.lstrip('#')
+
+            # Test an empty string
+            self.assertEqual(api.get_md_heading(''),None)
+
+            # Test for a string without headers
+            self.assertEqual(api.get_md_heading(test_text.replace('#','')),None)
+
+            # Note that we need to compare the test_text as input with
+            # test_text.strip() as output, since the string is stripped inside
+            # the method
+
+            # Test h1
+            self.assertEqual(api.get_md_heading('#' + test_text),
+{'type':1,'text_original':test_text.strip(),'text_slugified':slugify(test_text)})
+            self.assertEqual(api.get_md_heading('# ' + test_text),
+{'type':1,'text_original':test_text.strip(),'text_slugified':slugify(test_text)})
+
+            # Test h2
+            self.assertEqual(api.get_md_heading('##' + test_text),
+{'type':2,'text_original':test_text.strip(),'text_slugified':slugify(test_text)})
+            self.assertEqual(api.get_md_heading('## ' + test_text),
+{'type':2,'text_original':test_text.strip(),'text_slugified':slugify(test_text)})
+
+            # Test h3
+            self.assertEqual(api.get_md_heading('###' + test_text),
+{'type':3,'text_original':test_text.strip(),'text_slugified':slugify(test_text)})
+            self.assertEqual(api.get_md_heading('### ' + test_text),
+{'type':3,'text_original':test_text.strip(),'text_slugified':slugify(test_text)})
+
+            # Test h whith h > h3
+            self.assertEqual(api.get_md_heading('####'+slugify(test_text)),None)
+            self.assertEqual(api.get_md_heading('#### '+slugify(test_text)),None)
+
+            i+=1
+
+if __name__ == '__main__':
+    unitttest.main()
