55b166ba4604251942b1cf221a047af9af63fbc4
Franco Masotti <franco.masotti@live.com>
2019-04-14 18:21:33 +0200

    Updated version numbers.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='md_toc',
-    version='4.0.0',
+    version='5.0.0',
     packages=find_packages(exclude=['*tests*']),
     license='GPL',
     description='A utility that is able to generate a table of contents for a markdown file.',
