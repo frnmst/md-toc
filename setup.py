7c6fb2249f477d5270bb9cdd9bd74095165d79d0
Franco Masotti <franco.masotti@tutanota.com>
2023-11-30 17:03:26 +0100

    Fixes
    - Add setup classifiers
    - Move bandit configuration to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-#
-# setup.py
-#
-# Copyright (C) 2017-2023 Franco Masotti (see /README.md)
-#
-# This file is part of md-toc.
-#
-# md-toc is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# md-toc is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with md-toc.  If not, see <http://www.gnu.org/licenses/>.
-#
 r"""setup.py."""
 
 import setuptools
