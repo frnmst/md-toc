e1301647cb913096b1668fa99f08325e30f256b4
Franco Masotti <franco.masotti@tutanota.com>
2024-03-25 00:56:18 +0100

    Fix Makefile URL
    - Fix .project.mk Makefile URL

--- .project.mk
+++ .project.mk
@@ -1,6 +1,6 @@
 PROJECT_NAME := md-toc
 PYTHON_MODULE_NAME := md_toc
 
-MAKEFILE_SOURCE := https://software.franco.net.eu.org/frnmst/python-makefile/raw/branch/master/Makefile
+MAKEFILE_SOURCE := https://software.franco.net.eu.org/frnmst/python-makefile/raw/branch/master/Makefile.example
 bootstrap:
 	curl -o Makefile $(MAKEFILE_SOURCE)
